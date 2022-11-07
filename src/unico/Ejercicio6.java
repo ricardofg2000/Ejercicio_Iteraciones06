39fb7862d92595c73b991c0abbfed0cfb945d11d
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-07 10:04:24 +0100

    primer modelo ejercicio

---
+++ src/unico/Ejercicio6.java
@@ -0,0 +1,25 @@
+package unico;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		/*
+		 Escribir todos los múltiplos de 7 menores que 100.
+		 */
+
+		@SuppressWarnings("unused")
+		Scanner leer = new Scanner (System.in);
+		final int MULTIPLO = 7;
+		final int TOPE =  100;
+		int i;
+		
+		for (i=MULTIPLO; i <= TOPE; i++) {
+			if (i % MULTIPLO == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
