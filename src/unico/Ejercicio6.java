3f759098230db49665f999f938d7ab39dfb91820
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-15 20:45:50 +0100

    tabulación del ejercicio

--- src/unico/Ejercicio6.java
+++ src/unico/Ejercicio6.java
@@ -7,16 +7,16 @@ public class Ejercicio6 {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		/*
-		 Escribir todos los múltiplos de 7 menores que 100.
+		 * Escribir todos los múltiplos de 7 menores que 100.
 		 */
 
 		@SuppressWarnings("unused")
-		Scanner leer = new Scanner (System.in);
+		Scanner leer = new Scanner(System.in);
 		final int MULTIPLO = 7;
-		final int TOPE =  100;
+		final int TOPE = 100;
 		int i;
-		
-		for (i=MULTIPLO; i <= TOPE; i++) {
+
+		for (i = MULTIPLO; i <= TOPE; i++) {
 			if (i % MULTIPLO == 0) {
 				System.out.println(i);
 			}
